547f62f3cd804251c308e5cc8ddf6c46b9904305
Slowhand0309 <slowhand0309@gmail.com>
2016-08-24 23:37:05 +0900

    Update struct factory class

--- lib/sitar/types/struct_factory.rb
+++ lib/sitar/types/struct_factory.rb
@@ -12,14 +12,39 @@ module Sitar
         @structs[struct_name] = members
       end
 
-      def parse(members)
-        members.each do |member|
-          # TODO parse
+      def build
+        @structs.each do |k, v|
+          # TODO declare struct name
+          struct(parse(v))
         end
       end
 
-      def build
-        struct(members)
+      private
+
+      def parse(members)
+        parse_inner_struct(1, members)
+      end
+
+      def parse_inner_struct(count, members)
+        inners = []
+        count.times do
+          members.each do |member|
+            tmp = member
+            # Parse inner struct.
+            type = member.split(' ')[0]
+            inner_struct = @structs[type]
+            if inner_struct
+              value = member.split(' ')[1]
+              count = 1
+              if /\[(\d+)\]/ =~ value
+                count = $1.to_i
+              end
+              tmp << parse_inner_struct(count, inner_struct)
+            end
+            inners << tmp
+          end
+        end
+        inners.flatten
       end
 
     end
